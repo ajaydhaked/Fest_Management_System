7150a5f7b2a007335df026a3858242c5ab72ca90
Ajay Kumar Dhakar <ajaydhaker2002@kgpian.iitkgp.ac.in>
2024-02-26 19:12:18 +0530

    events and routes added

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-
 @app.get("/")
 def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/events")
+def read_root(request: Request):
+    return templates.TemplateResponse("events.html", {"request": request})
+
+@app.get("/about")
+def read_root(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
